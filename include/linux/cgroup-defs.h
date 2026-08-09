50754b935fd264f71b48010ccdf6eb2a1f0d8264
Anya1014CN <han.cf@foxmail.com>
2026-08-09 22:23:32 +0800

    cgroup: support cpuset v2 mode for Android

--- include/linux/cgroup-defs.h
+++ include/linux/cgroup-defs.h
@@ -66,6 +66,8 @@ enum {
 enum {
 	CGRP_ROOT_NOPREFIX	= (1 << 1), /* mounted subsystems have no named prefix */
 	CGRP_ROOT_XATTR		= (1 << 2), /* supports extended attributes */
+	/* Enable cpuset controller in v1 cgroup to use v2 behavior. */
+	CGRP_ROOT_CPUSET_V2_MODE = (1 << 3),
 };
 
 /* cftype->flags */
